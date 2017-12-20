766f5eb5c50e5060b3bd201c09a4e70a4cbbddad
eseulean <eseulean@bloomberg.net>
2017-12-20 16:20:34 +0000

    scrapped exchange

--- analytics.py
+++ analytics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import json
 from datetime import date
 
 def cleaned_dataset():
@@ -30,12 +31,26 @@ def get_historical_data(top100, ticker, start, end):
         (top100['Date'] >= start) &
         (top100['Date'] <= end)][['Date', 'Volume', 'Close', 'Market-Cap']]
 
+def per_exchange_currencies(exchange_names):
+    exchanges = json.load(open('dict.json'))
+    all_currencies = set()
+    for name in exchange_names:
+        all_currencies = all_currencies | set(exchanges[name])
+
+    return all_currencies
+
+# get all from top 100 that are traded at the exchanges passed in as parameter
+def filtered_data(top100, exchange_names):
+    top100 = top100[top100['Coin'].isin(exchange_names)]
+    top100 = top100[top100['Date'] == '2017-12-18']
 
 
 dataset = cleaned_dataset()
 #top10_by_market_cap(dataset)
-current = get_historical_data(dataset, 'litecoin', '2017-12-16', '2017-12-18')
+# current = get_historical_data(dataset, 'litecoin', '2017-12-16', '2017-12-18')
 
-hist = get_historical_data(dataset, 'litecoin', '2017-12-01', '2017-12-01')
-current['diff'] = (current['Close'] - hist['Close'].values[0]) / hist['Close'].values[0]
-print(current)
+# hist = get_historical_data(dataset, 'litecoin', '2017-12-01', '2017-12-01')
+# current['diff'] = (current['Close'] - hist['Close'].values[0]) / hist['Close'].values[0]
+# print(current)
+
+filtered_data(dataset, per_exchange_currencies(['Poloniex', 'Bitfinex']))
