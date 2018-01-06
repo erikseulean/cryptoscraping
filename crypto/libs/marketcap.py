3fa95f97e3ef8508aa30979ade28667f759265af
Erik Cristian <erikseulean@gmail.com>
2018-01-06 13:09:40 +0000

    add extra implementations

--- crypto/libs/marketcap.py
+++ crypto/libs/marketcap.py
@@ -47,39 +47,51 @@ class MarketCap:
         
         return nlargest(number_of_coins, top)
 
-    def top(self, number_of_coins, as_of_date=None):
-        if as_of_date is None:
-            return self._top_today_(number_of_coins)
-        
-        def transform(dataset):
+    def _transform_(self, dataset):
             result = []
             for index, row in dataset.iterrows():
                 result.append((row['Market-Cap'], row['Coin']))
             return result
 
+    def top(self, number_of_coins, as_of_date=None):
+        if as_of_date is None:
+            return self._top_today_(number_of_coins)
+        
         data = self.dataset[(self.dataset['Date'] == as_of_date)]
         data = data.sort_values('Market-Cap', ascending=False)
-        return transform(data[0:number_of_coins])
+        return self._transform_(data[0:number_of_coins])
 
-    def _range_today_(self, lower_bound, upper_bound):
+    def _range_today_(self, lower_bound = None, upper_bound = None):
         current = get_current_values()
         return [
             (float(data['market_cap_usd']), coin) for coin, data in current.items() 
             if data['market_cap_usd'] and (
-                float(data['market_cap_usd']) >= lower_bound and float(data['market_cap_usd']) <= upper_bound
+                (float(data['market_cap_usd']) >= lower_bound if lower_bound else True) and 
+                (float(data['market_cap_usd']) <= upper_bound if upper_bound else True)
             )
         ]
-
+    
+    def _historical_range_(self, as_of_date, lower_bound = None, upper_bound = None):
+        data = self.dataset[self.dataset['Date'] == as_of_date]
+        if lower_bound:
+            data = data[data['Market-Cap'] >= lower_bound]
+        if upper_bound:
+            data = data[data['Market-Cap'] <= upper_bound]           
+        
+        return self._transform_(data)        
 
     def range(self, lower_bound, upper_bound, as_of_date = None):
         if as_of_date is None:
             return self._range_today_(lower_bound, upper_bound)
+        return self._historical_range_(as_of_date, lower_bound, upper_bound)
+
         
     def smaller(self, upper_bound, as_of_date = None):
-        pass
+        if as_of_date is None:
+            return self._range_today_(upper_bound = upper_bound)
+        return self._historical_range_(as_of_date, upper_bound = upper_bound)
 
     def bigger(self, lower_bound, as_of_date = None):
-        pass
-
-    def between(self, lower_bound, upper_bound, as_of_date = None):
-        pass
+        if as_of_date is None:
+            return self._range_today_(lower_bound = lower_bound)
+        return self._historical_range_(as_of_date, lower_bound = lower_bound)
